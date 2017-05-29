9a50c5718b5024f62c03fae62a259cb56ba35e06
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-05-29 13:19:24 +0300

    Added primer-container

---
+++ addon/components/primer-container/component.js
@@ -0,0 +1,98 @@
+import Component from 'ember-component';
+import layout from './template';
+import computed from 'ember-computed';
+import box from 'ember-primer/utils/box-expression';
+const { keys } = Object;
+const Container =  Component.extend({
+  tagName: '',
+
+  layout,
+
+  xScale: null,
+
+  yScale: null,
+
+  width: null,
+
+  height: null,
+
+  margin: '0',
+  padding: '0',
+
+  localizedXScale: computed('xScale', 'rect', {
+    get() {
+      let { xScale, rect } = this.getProperties('xScale', 'rect');
+      return xScale.copy().domain([0, rect.width]);
+    }
+  }),
+
+  localizedYScale: computed('yScale', 'rect', {
+    get() {
+      let { yScale, rect } = this.getProperties('yScale', 'rect');
+      return yScale.copy().range([rect.height, 0]);
+    }
+  }),
+
+  rect: computed('width', 'height', 'margin', {
+    get() {
+      let outerHeight = this.get('height') || 10;
+      let outerWidth = this.get('width') || 10;
+      let margin = box(this.get('margin'));
+      let padding = box(this.get('padding'));
+
+      let innerWidth = outerWidth - margin.left - margin.right;
+      let innerHeight = outerHeight - margin.top - margin.bottom;
+      let width = innerWidth - padding.left - padding.right;
+      let height = innerHeight - padding.top - padding.bottom;
+
+      let rect = {
+        padding,
+        margin,
+        outerWidth, outerHeight,
+        innerWidth, innerHeight,
+        width, height,
+        top: margin.top + padding.top,
+        left: margin.left + padding.left
+        // right: margin.left + padding.left,
+      };
+
+      // let height = this.get('height') || 10;
+      // let width = this.get('width') || 10;
+      // let margin = box(this.get('margin'));
+      // let padding = box(this.get('padding'));
+
+      // let innerWidth = width - padding.left - padding.right;
+      // let innerHeight = width - padding.top - padding.bottom;
+
+      // // console.log(innerWidth, innerHeight);
+
+      // let rect = {
+      //   margin,
+      //   top: margin.top + padding.top,
+      //   left: margin.left + padding.left,
+      //   bottom: innerHeight - margin.top,
+      //   right: innerWidth - margin.right,
+      //   outerWidth: width,
+      //   outerHeight: height,
+      //   innerWidth,
+      //   innerHeight,
+      //   height: innerHeight - margin.top - margin.bottom,
+      //   width: innerWidth - margin.left - margin.right
+      // };
+
+      // keys(rect).forEach((key) => {
+      //   if (rect[key] < 0) {
+      //     rect[key] = 0;
+      //   }
+      // });
+
+      return rect;
+    }
+  })
+});
+
+Container.reopenClass({
+  positionalParams: ['xScale', 'yScale']
+});
+
+export default Container;
---
+++ addon/mixins/resizeable-container.js
@@ -0,0 +1,48 @@
+import DOM from 'ember-lifeline/mixins/dom';
+import Mixin from 'ember-metal/mixin';
+import run from 'ember-runloop';
+
+export default Mixin.create(DOM, {
+
+  minHeight: 50,
+
+  minWidth: 50,
+
+  width: null,
+
+  height: null,
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    if (this.get('width') === null) {
+      this.addEventListener(window, `resize.${ this.elementId }`, this.didResize);
+      run.scheduleOnce('afterRender', this, this.measureDimensions);
+    }
+  },
+
+  didResize() {
+    run.throttle(this, this.measureDimensions, 16);
+  },
+
+  /**
+   * Calculates dimensions of this container. This forces the browser to calculate
+   * styles, which is slow, so don't do this too often or you'll create jank.
+   *
+   * @public
+   */
+  measureDimensions() {
+    if (!this.element) {
+      return;
+    }
+
+    let { minWidth, minHeight } = this.getProperties('minWidth', 'minHeight');
+
+    let rect = this.element.getBoundingClientRect();
+    this.setProperties({
+      width: Math.max(rect.width, minWidth),
+      height: Math.max(rect.height, minHeight)
+    });
+  }
+
+});
