a3d85207fcf13b32d4e7ac3c91459f103f3649ab
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-05-31 14:16:21 +0300

    Refactored cursor to only update if data value actually changes

--- addon/components/primer-cursor/component.js
+++ addon/components/primer-cursor/component.js
@@ -20,6 +20,8 @@ export default Component.extend(DOM, {
 
   isActive: false,
 
+  _lastPosition: [],
+
   didInsertElement() {
     let svg = this.element.closest('svg');
     this.addEventListener(svg, 'mousemove', this.handleMouseMove);
@@ -42,20 +44,35 @@ export default Component.extend(DOM, {
   _mouseMove(event) {
     let { offsetY: y, offsetX: x } = event;
     let { xScale, yScale, values } = this.getProperties('xScale', 'yScale', 'values');
+    let { xOffset, yOffset } = this.getProperties('xOffset', 'yOffset');
 
     if (!xScale) {
       this.setProperties({ isActive: true, x, y });
       this.sendAction('change', [x, y]);
     } else {
-      let valueAtX = xScale.invert(x);
+      let valueAtX = xScale.invert(x - xOffset);
       // let valueAtY = yScale.invert(y);
 
-      let index = closest(valueAtX, values);
+      // 1. Find closest data point to current cursor X
+      let index = closest(Number(valueAtX), values.map((d) => Number(d[0])));
 
+      // 2. Find actual data point for x,y based on index of 1.
       let [xValue, yValue] = values[index] || [0, 0];
 
-      this.setProperties({ isActive: true, x: xScale(xValue), y: yScale(yValue) });
-      this.sendAction('change', [xValue, yValue]);
+      // 3. Scale values back to x,y
+      let xPointer = xScale(xValue);
+      let yPointer = yScale(yValue);
+
+      // 4. Only update and action if position has actually changed
+      let [xLast, yLast] = this._lastPosition;
+      if (xLast !== xPointer || yLast !== yPointer) {
+        this.setProperties({ isActive: true, x: xPointer, y: yPointer });
+        this.sendAction('change', [xValue, yValue], [xPointer, yPointer]);
+        this.sendAction('_change', [xValue, yValue], [xPointer, yPointer]);
+
+        this._lastPosition = [xPointer, yPointer];
+      }
+
     }
   }
 });
