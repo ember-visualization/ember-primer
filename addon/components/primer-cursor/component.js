c8b562c740cbcb3a90df1490f4aa0a65cec46c95
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-06-02 11:53:42 +0300

    Refactored cursor

--- addon/components/primer-cursor/component.js
+++ addon/components/primer-cursor/component.js
@@ -1,12 +1,14 @@
 import Component from 'ember-component';
-import DOM from 'ember-lifeline/mixins/dom';
-import closest from 'ember-primer/utils/binary-closest-search';
+import closestPoint from 'ember-primer/utils/find-closest-cursor-points';
 import run from 'ember-runloop';
 import layout from './template';
+import { ascending } from 'd3-array';
 
-export default Component.extend(DOM, {
+export default Component.extend({
   layout,
   tagName: 'g',
+
+  classNames: ['Primer-Cursor'],
 
   xScale: null,
 
@@ -24,9 +26,16 @@ export default Component.extend(DOM, {
 
   didInsertElement() {
     let svg = this.element.closest('svg');
-    this.addEventListener(svg, 'mousemove', this.handleMouseMove);
-    this.addEventListener(svg, 'mouseleave', this.handleMouseLeave);
-    this.addEventListener(svg, 'mouseenter', this.handleMouseEnter);
+    svg.addEventListener('mousemove', this.handleMouseMove.bind(this));
+    svg.addEventListener('mouseleave', this.handleMouseLeave.bind(this));
+    svg.addEventListener('mouseenter', this.handleMouseEnter.bind(this));
+  },
+
+  willRemoveElement() {
+    let svg = this.element.closest('svg');
+    svg.removeEventListener('mousemove', this.handleMouseMove);
+    svg.removeEventListener('mouseleave', this.handleMouseLeave);
+    svg.removeEventListener('mouseenter', this.handleMouseEnter);
   },
 
   handleMouseMove(event) {
@@ -41,6 +50,10 @@ export default Component.extend(DOM, {
     this.set('isActive', true);
   },
 
+  _convertCursorCoordinatesToDataCoordinates([x, y]) {
+    return this.getAttr('convertCursorCoordinatesToDataCoordinates')([x, y]);
+  },
+
   _mouseMove(event) {
     let { offsetY: y, offsetX: x } = event;
     let { xScale, yScale, values } = this.getProperties('xScale', 'yScale', 'values');
@@ -50,25 +63,13 @@ export default Component.extend(DOM, {
       this.setProperties({ isActive: true, x, y });
       this.sendAction('change', [x, y]);
     } else {
-      let valueAtX = xScale.invert(x - xOffset);
-      // let valueAtY = yScale.invert(y);
+      let [[xPointer, yPointer], [xValue, ...yValues]] = closestPoint([x, y], [xOffset, yOffset], xScale, yScale, values);
 
-      // 1. Find closest data point to current cursor X
-      let index = closest(Number(valueAtX), values.map((d) => Number(d[0])));
-
-      // 2. Find actual data point for x,y based on index of 1.
-      let [xValue, yValue] = values[index] || [0, 0];
-
-      // 3. Scale values back to x,y
-      let xPointer = xScale(xValue);
-      let yPointer = yScale(yValue);
-
-      // 4. Only update and action if position has actually changed
       let [xLast, yLast] = this._lastPosition;
       if (xLast !== xPointer || yLast !== yPointer) {
         this.setProperties({ isActive: true, x: xPointer, y: yPointer });
-        this.sendAction('change', [xValue, yValue], [xPointer, yPointer]);
-        this.sendAction('_change', [xValue, yValue], [xPointer, yPointer]);
+        this.sendAction('change', [xValue, yValues], [xPointer, yPointer]);
+        this.sendAction('_change', [xValue, yValues], [xPointer, yPointer]);
 
         this._lastPosition = [xPointer, yPointer];
       }
