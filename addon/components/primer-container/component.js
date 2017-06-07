74d4616cbaf6c10fcf4e8f7580aed8307f3ce6fa
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-06-07 16:54:38 +0200

    Reverted margin changes

--- addon/components/primer-container/component.js
+++ addon/components/primer-container/component.js
@@ -41,53 +41,53 @@ const Container =  Component.extend({
       let outerHeight = this.get('height') || 10;
       let outerWidth = this.get('width') || 10;
       let margin = box(this.get('margin'));
-      let padding = box(this.get('padding'));
+      // let padding = box(this.get('padding'));
+
+      // let innerWidth = outerWidth - margin.left - margin.right;
+      // let innerHeight = outerHeight - margin.top - margin.bottom;
+      // let width = innerWidth - padding.left - padding.right;
+      // let height = innerHeight - padding.top - padding.bottom;
+
+      // let rect = {
+      //   padding,
+      //   margin,
+      //   outerWidth, outerHeight,
+      //   innerWidth, innerHeight,
+      //   width, height,
+      //   top: margin.top + padding.top,
+      //   left: margin.left + padding.left
+      //   // right: margin.left + padding.left,
+      // };
+
+      // // let height = this.get('height') || 10;
+      // // let width = this.get('width') || 10;
+      // // let margin = box(this.get('margin'));
+      // // let padding = box(this.get('padding'));
 
       let innerWidth = outerWidth - margin.left - margin.right;
       let innerHeight = outerHeight - margin.top - margin.bottom;
-      let width = innerWidth - padding.left - padding.right;
-      let height = innerHeight - padding.top - padding.bottom;
 
       let rect = {
-        padding,
         margin,
-        outerWidth, outerHeight,
-        innerWidth, innerHeight,
-        width, height,
-        top: margin.top + padding.top,
-        left: margin.left + padding.left
-        // right: margin.left + padding.left,
+        top: margin.top,
+        left: margin.left,
+        bottom: innerHeight - margin.top,
+        right: innerWidth - margin.right,
+        outerWidth,
+        outerHeight,
+        innerWidth,
+        innerHeight,
+
+        // Dupes
+        height: innerHeight,
+        width: innerWidth
       };
 
-      // let height = this.get('height') || 10;
-      // let width = this.get('width') || 10;
-      // let margin = box(this.get('margin'));
-      // let padding = box(this.get('padding'));
-
-      // let innerWidth = width - padding.left - padding.right;
-      // let innerHeight = width - padding.top - padding.bottom;
-
-      // // console.log(innerWidth, innerHeight);
-
-      // let rect = {
-      //   margin,
-      //   top: margin.top + padding.top,
-      //   left: margin.left + padding.left,
-      //   bottom: innerHeight - margin.top,
-      //   right: innerWidth - margin.right,
-      //   outerWidth: width,
-      //   outerHeight: height,
-      //   innerWidth,
-      //   innerHeight,
-      //   height: innerHeight - margin.top - margin.bottom,
-      //   width: innerWidth - margin.left - margin.right
-      // };
-
-      // keys(rect).forEach((key) => {
-      //   if (rect[key] < 0) {
-      //     rect[key] = 0;
-      //   }
-      // });
+      keys(rect).forEach((key) => {
+        if (rect[key] < 0) {
+          rect[key] = 0;
+        }
+      });
 
       return rect;
     }
@@ -95,7 +95,8 @@ const Container =  Component.extend({
 
   actions: {
     updateCursorPosition([xValue, yValue], [xCursor, yCursor]) {
-      this.setProperties({ xCursor, yCursor });
+      this.setProperties({ cursorPosition: [xCursor, yCursor] });
+      this.sendAction('_cursorChangedPosition', [xValue, yValue], [xCursor, yCursor]);
     }
   }
 });
