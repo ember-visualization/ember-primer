4bd783da1e6cc6354663c7d7599980a3597a462a
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-06-07 16:53:47 +0200

    Updated resize container to use mutations to trigger resize events

--- addon/mixins/resizeable-container.js
+++ addon/mixins/resizeable-container.js
@@ -1,6 +1,8 @@
 import Mixin from 'ember-metal/mixin';
 import run from 'ember-runloop';
 
+const MutObserver = self.window.MutationObserver || self.window.WebKitMutationObserver;
+
 export default Mixin.create({
 
   minHeight: 50,
@@ -11,17 +13,66 @@ export default Mixin.create({
 
   height: null,
 
+  init() {
+    this._super();
+
+    this.runloopAwareResize = () => {
+      run.join(this, this.didResize);
+    };
+  },
+
+  addGlobalEvents() {
+    window.addEventListener('resize', this.runloopAwareResize, { passive: true });
+    window.addEventListener('orientationchange', this.runloopAwareResize, { passive: true });
+  },
+
+  removeGlobalEvents() {
+    window.removeEventListener('resize', this.runloopAwareResize);
+    window.removeEventListener('orientationchange', this.runloopAwareResize);
+  },
+
+  startObservingDomMutations() {
+    if (MutObserver) {
+      this.mutationObserver = new MutObserver((mutations) => {
+        if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
+          this.runloopAwareResize();
+        }
+      });
+      this.mutationObserver.observe(this.element, { childList: true, subtree: true });
+    } else {
+      this.element.addEventListener('DOMNodeInserted', this.runloopAwareResize, false);
+      this.element.addEventListener('DOMNodeRemoved', this.runloopAwareResize, false);
+    }
+  },
+
+  stopObservingDomMutations() {
+    if (MutObserver) {
+      if (this.mutationObserver) {
+        this.mutationObserver.disconnect();
+        this.mutationObserver = null;
+      }
+    } else {
+      if (this.element) {
+        this.element.removeEventListener('DOMNodeInserted', this.runloopAwareReposition);
+        this.element.removeEventListener('DOMNodeRemoved', this.runloopAwareReposition);
+      }
+    }
+  },
+
   didInsertElement() {
     this._super(...arguments);
 
     if (this.get('width') === null) {
-      window.addEventListener(`resize.${ this.elementId }`, this.didResize.bind(this));
+      this.addGlobalEvents();
+      this.startObservingDomMutations();
+
+      // window.addEventListener(`resize.${ this.elementId }`, this.didResize.bind(this));
       run.scheduleOnce('render', this, this.measureDimensions);
     }
   },
 
-  willRemoveElement() {
-    window.removeEventListener(`resize.${ this.elementId }`, this.didResize);
+  willDestroyElement() {
+    this.removeGlobalEvents();
   },
 
   didResize() {
