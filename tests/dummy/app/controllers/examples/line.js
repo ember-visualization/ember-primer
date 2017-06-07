20e313a1e073cc469f741fe3bb5c9f8a16caca29
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-06-07 16:52:27 +0200

    Updated docs

--- tests/dummy/app/controllers/examples/line.js
+++ tests/dummy/app/controllers/examples/line.js
@@ -2,14 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  tooltipPositionX: 0,
-  tooltipPositionY: 0,
+  cursorPosition: [0, 0],
 
   actions: {
     updateToolTipPosition([x, y]) {
       this.setProperties({
-        tooltipPositionX: x,
-        tooltipPositionY: y
+        cursorPosition: [x, y]
       });
     }
   }
