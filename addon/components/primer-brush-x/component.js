0a2f4b9b27fb7de3a01f9167e5f88ecfcbf2e867
zigahertz <zach@flood.io>
2018-05-08 11:19:42 -0700

    moves brush to initial selection

--- addon/components/primer-brush-x/component.js
+++ addon/components/primer-brush-x/component.js
@@ -3,7 +3,7 @@ import computed from 'ember-computed'
 import run from 'ember-runloop'
 import layout from './template'
 
-import { brushX } from 'd3-brush'
+import { brushX, brushSelection } from 'd3-brush'
 import { axisBottom } from 'd3-axis'
 import { select, event } from 'd3-selection'
 
@@ -25,6 +25,8 @@ export default Component.extend({
   }),
 
   drawBrush() {
+    console.log('drawing')
+
     let { xScale, rect, handleSize, selection } = this.getProperties(
       'xScale',
       'rect',
@@ -32,22 +34,38 @@ export default Component.extend({
       'selection',
     )
 
+    let brushElement = select(this.element)
+    let mappedSelection = selection ? selection.map(xScale) : null
+
     let brush = brushX()
       .extent([[0, 0], [rect.width, handleSize]])
       .handleSize(handleSize)
-      .on('end', () => {
-        let { selection: eventSelection } = event
-        let extent = eventSelection.map(xScale.invert, xScale)
-        run.next(this, () => {
-          this.sendAction('_change', extent, !eventSelection)
-        })
-      })
+    // .on('end', () => {
 
-    let moveSelection = selection.length ? selection.map(xScale) : null
+    // if (selection[0] !== extent[0] || selection[1] !== extent[1]) {
+    // }
+    // console.log(selection, extent)
+    // debugger
+    // })
 
-    select(this.element)
+    brushElement
       .call(brush)
+      .call(brush.move, mappedSelection)
       .attr('class', 'brush')
-      .call(brush.move, moveSelection)
+
+    brush.on('end', () => {
+      let { selection: eventSelection } = event
+      let extent = eventSelection ? eventSelection.map(xScale.invert, xScale) : xScale.domain()
+      run.next(this, () => {
+        this.sendAction('_change', extent, !eventSelection)
+      })
+    })
+
+    // let [rangeStart, rangeEnd] = xScale.range()
+
+    // console.log(mappedSelection)
+
+    // if (mappedSelection[0] !== rangeStart || mappedSelection[1] !== rangeEnd) {
+    // }
   },
 })
